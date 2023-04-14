09a64ad90227a732079923665c4a024663281cd8
Jeromey Mathews <jeromey.mathews@gmail.com>
2023-04-14 10:57:30 -0500

    Testing the commits

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-const app = express()
+const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
